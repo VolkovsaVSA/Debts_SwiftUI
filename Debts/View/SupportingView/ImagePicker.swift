f90a6dceeb7128651d365471372882ea8006c801
Sergei Volkov <volkovsa@vsa.su>
2021-12-12 00:14:41 +0500

    create image compressor

--- Debts/View/SupportingView/ImagePicker.swift
+++ Debts/View/SupportingView/ImagePicker.swift
@@ -42,19 +42,60 @@ struct ImagePicker: UIViewControllerRepresentable {
             if provider.canLoadObject(ofClass: UIImage.self) {
                 provider.loadObject(ofClass: UIImage.self) { image, _ in
                     DispatchQueue.main.async {
-
-                        if let unwrapImage = image as? UIImage {
-                            self.parent.image = unwrapImage.jpegData(compressionQuality: 0.01)
+                        guard let unwrapImage = image as? UIImage else { return }
+                        ImageCompressor.compress(image: unwrapImage, maxByte: 50000) { resizeImage in
+                            guard let compressedImage = resizeImage?.jpegData(compressionQuality: 0.1) else { return }
+                            print(compressedImage.description)
+                            self.parent.image = compressedImage
                         }
-
-                        DispatchQueue.main.async {
-                            EditDebtorDataViewModel.shared.refreshID = UUID()
-                        }
-
                     }
-                    
                 }
             }
+            
+        }
+        
+    }
+    
+}
+
+
+struct ImageCompressor {
+    static func compress(image: UIImage, maxByte: Int,
+                         completion: @escaping (UIImage?) -> ()) {
+        DispatchQueue.global(qos: .userInitiated).async {
+            guard let currentImageSize = image.jpegData(compressionQuality: 1.0)?.count else {
+                return completion(nil)
+            }
+        
+            var iterationImage: UIImage? = image
+            var iterationImageSize = currentImageSize
+            var iterationCompression: CGFloat = 1.0
+        
+            while iterationImageSize > maxByte && iterationCompression > 0.01 {
+                let percantageDecrease = getPercantageToDecreaseTo(forDataCount: iterationImageSize)
+            
+                let canvasSize = CGSize(width: image.size.width * iterationCompression,
+                                        height: image.size.height * iterationCompression)
+                UIGraphicsBeginImageContextWithOptions(canvasSize, false, image.scale)
+                defer { UIGraphicsEndImageContext() }
+                image.draw(in: CGRect(origin: .zero, size: canvasSize))
+                iterationImage = UIGraphicsGetImageFromCurrentImageContext()
+            
+                guard let newImageSize = iterationImage?.jpegData(compressionQuality: 1.0)?.count else {
+                    return completion(nil)
+                }
+                iterationImageSize = newImageSize
+                iterationCompression -= percantageDecrease
+            }
+            completion(iterationImage)
+        }
+    }
+
+    private static func getPercantageToDecreaseTo(forDataCount dataCount: Int) -> CGFloat {
+        switch dataCount {
+        case 0..<3000000: return 0.05
+        case 3000000..<10000000: return 0.1
+        default: return 0.2
         }
     }
 }
